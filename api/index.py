60773a95371fdd7e31edd4fe81be8012cc161f41
Dangelo <dangelo.jun@gmail.com>
2023-01-09 01:38:15 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,35 +6,68 @@ import json
 
 def list_split(items, n):
     return [items[i:i + n] for i in range(0, len(items), n)]
+    
+    
+    
 def getdata(name):
     gitpage = requests.get("https://github.com/" + name)
     data = gitpage.text
+    # print('---------------------------------------')
+#     print(data)
+#     print('---------------------------------------')
     datadatereg = re.compile(r'data-date="(.*?)" data-level')
-    datacountreg = re.compile(r'data-count="(.*?)" data-date')
+    # datacountreg = re.compile(r'data-count="(.*?)" data-date')
+    # datacountreg = re.compile(r'>"(.*?)" contribution on')
+    datacountreg = re.compile(r'(\d+)\s+contributio')
+    datacountreg = re.compile(r'ry="2">(.+?) contributions')
     datadate = datadatereg.findall(data)
+    print(datadate)
+ 
     datacount = datacountreg.findall(data)
+
+    datacount = [x if x !='No' else '0' for x in datacount]
+    print(datacount)  
+    
     datacount = list(map(int, datacount))
+    print(datacount)
     contributions = sum(datacount)
+    print(contributions)
     datalist = []
+
     for index, item in enumerate(datadate):
         itemlist = {"date": item, "count": datacount[index]}
-#         itemlist = {"date": item, "count": 0}
         datalist.append(itemlist)
     datalistsplit = list_split(datalist, 7)
     returndata = {
         "total": contributions,
-        "total": 0,
         "contributions": datalistsplit
     }
     return returndata
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        path = self.path
-        user = path.split('?')[1]
-        data = getdata(user)
-        self.send_response(200)
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        self.wfile.write(json.dumps(data).encode('utf-8'))
-        return
+    
+
+    
+def findbug():
+    path='https://github.com/api?Dan-Sylvain'
+    print(path)
+    user = path.split('?')[1]
+    print(user)
+    data = getdata(user)
+    print(data)
+   
+  
+getdata('Dan-Sylvain')   
+# findbug()
+        
+    
+    
+# class handler(BaseHTTPRequestHandler):
+#     def do_GET(self):
+#         path = self.path
+#         user = path.split('?')[1]
+#         data = getdata(user)
+#         self.send_response(200)
+#         self.send_header('Access-Control-Allow-Origin', '*')
+#         self.send_header('Content-type', 'application/json')
+#         self.end_headers()
+#         self.wfile.write(json.dumps(data).encode('utf-8'))
+#         return
